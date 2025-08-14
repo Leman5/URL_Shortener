dfd3460e7b7ecaea49f6253d59ff340b851e3425
Leman5 <leman5kirme5@gmail.com>
2025-08-14 17:57:47 +1000

    Implement FastAPI backend and Streamlit client

--- urlshortener.py
+++ urlshortener.py
@@ -1,16 +1,22 @@
 import streamlit as st
+import requests
+
+API_ENDPOINT = "http://localhost:8000/shorten"
 
 st.header("BESTest URL Shortener")
 
-def shorten_url(url):
-    return url
+url = st.text_input("Enter the URL to shorten:", placeholder="https://example.com")
 
-url = st.text_input("Enter the URL to shorten:",placeholder="https://example.com")
 if st.button("Submit"):
     if url:
-        short_url = shorten_url(url)
-        st.success(f"Shortened URL: {short_url}")
+        try:
+            response = requests.post(API_ENDPOINT, json={"url": url})
+            if response.status_code == 200:
+                short_url = response.json().get("short_url")
+                st.success(f"Shortened URL: {short_url}")
+            else:
+                st.error("Failed to shorten URL.")
+        except requests.exceptions.RequestException as e:
+            st.error(f"Error: {e}")
     else:
         st.error("Please enter a valid URL.")
-
-
